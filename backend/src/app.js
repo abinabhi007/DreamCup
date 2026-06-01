12f55d7319cd77ef7f225bb93edae8c8160cb6aa
Abin <abinhn62@gmail.com>
2026-06-01 10:58:48 +0530

    chore: restructure to monorepo with frontend and backend directories

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+
+
+const app = express();
+app.use("/api/auth", authRoutes);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "DreamCup API Running 🚀"
+  });
+});
+
+
+app.use("/api/auth", authRoutes);
+
+module.exports = app;
