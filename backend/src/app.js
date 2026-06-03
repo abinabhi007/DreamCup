a00e0cd8aa38e82e60a3e8e980d46d23c9cd9648
Abin <abinhn62@gmail.com>
2026-06-03 18:53:01 +0530

    updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,15 @@ const teamRoutes = require("./routes/teamRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://dreamcup.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
