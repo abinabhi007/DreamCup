55059f68eb47b25f30201aeb4e68c27da5772453
Abin <abinhn62@gmail.com>
2026-06-12 16:53:32 +0530

    otp production fix

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,13 +1,17 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-  // Create a transporter using Gmail SMTP
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 10000, // 10 seconds max to connect
+    greetingTimeout: 10000,
+    socketTimeout: 15000,
   });
 
   const mailOptions = {
